908711c081d6497ce349a38400e72c084ec1aab1
Lucas Bach <lborgesbach@gmail.com>
2024-02-23 10:38:52 -0300

    criando function

---
+++ index.js
@@ -0,0 +1,9 @@
+//Gerador de senhas
+
+function gerarSenha(comprimento){
+    let caracteres = 
+    'ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz0123456789!@#$%¨&*()-_/*-+<>?{}[]~^'
+    
+    let senha = ''
+
+}
